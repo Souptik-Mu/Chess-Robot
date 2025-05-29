b389ccf6ac66ac0e382b65224878878ea87f1438
Souptik Mullick <souptikmullick0692@gmail.com>
2025-05-29 17:25:20 +0530

    Move ESPPP.ino to firmware/MatrixDriver.ino

--- firmware/MatrixDriver.ino
+++ firmware/MatrixDriver.ino
@@ -6,16 +6,16 @@ const uint8_t greenClockPin = 32;
 const uint8_t redDataPin = 13;
 const uint8_t redClockPin = 12;
 //buttons
-const uint8_t buttonE = 14;
-const uint8_t buttonW = 27;
-const uint8_t buttonB = 26;
-const uint8_t buttonH = 25;
+const uint8_t buttonW = 14;
+const uint8_t buttonH = 27;
+const uint8_t buttonR = 26;
+const uint8_t buttonB = 25;
 
 
 bool lastButtonWState = HIGH;
-bool lastButtonBState = HIGH;
 bool lastButtonHState = HIGH;
-
+bool lastButtonRState = HIGH;
+bool lastButtonBState = HIGH;
 
 
 bool green[8][8] = {
@@ -39,6 +39,7 @@ bool red[8][8] = {
     {1,1,1,1,1,1,1,1},
     {1,1,1,1,1,1,1,1}
 };
+
 void updateDisplayMatrix() {
   for(uint8_t row = 0; row < 8; row++){
       uint8_t greenByte = 0 ,redByte = 0;
@@ -58,11 +59,11 @@ void updateDisplayMatrix() {
 
 }
 
-void clear(){
+void SetAll(int R,int G){
   for(int i = 0; i < 8; i++){
     for(int j = 0; j < 8; j++){
-      green[i][j] = 0;
-      red[i][j] = 0;
+      green[i][j] = R;
+      red[i][j] = G;
     }
   }
 }
@@ -72,12 +73,9 @@ void handleButtonW() {
     while (Serial.available() == 0);  // Wait for move
     String move = Serial.readStringUntil('\n');
     move.trim();
+
     if(move == "non")
-    {
-      for(int i = 0; i < 8; i++)
-        for(int j = 0; j < 8; j++)
-          green[i][j] = 1;  //!?? should be red but somehow it works. 
-    }
+      SetAll(1,0);
     else
     {
       int fromRow = move.charAt(2) - 'a';
@@ -91,35 +89,36 @@ void handleButtonW() {
 }
 
 void handleButtonB() {
-  clear();
+  SetAll(0,0);
   Serial.println("read");
 }
 
+void handleButtonR() {
+  SetAll(0,1);
+  Serial.println("resign");
+}
+
 void handleButtonH() {
-    Serial.println("help");
+  Serial.println("help");
 
-    while (Serial.available() == 0);  // Wait for move
-    String moves = Serial.readStringUntil('\n');
-    moves.trim();
-    if(moves == "non")
-    {
-      for(int i = 0; i < 8; i++)
-        for(int j = 0; j < 8; j++)
-          green[i][j] = 1;  //!?? should be red but somehow it works. 
-    }
-    else
-    {
-      int srcRow = moves.charAt(0) - 'a';
-      int srcCol = 8 - (moves.charAt(1) - '0');
-      green[srcRow][srcCol] = 1; //! ulto a6, should be red. circuit works.
+  while (Serial.available() == 0);  // Wait for move
+  String moves = Serial.readStringUntil('\n');
+  moves.trim();
+  if(moves == "non")
+    SetAll(1,0);  
+  else
+  {
+    int srcRow = moves.charAt(0) - 'a';
+    int srcCol = 8 - (moves.charAt(1) - '0');
+    green[srcRow][srcCol] = 1; //! ulto a6, should be red. circuit works.
 
-      for(int i = 2; i < moves.length() ; i += 2)
-      {
-        int dstRow = moves.charAt(i) - 'a';
-        int dstCol = 8 - (moves.charAt(i+1) - '0');
-          red[dstRow][dstCol] = 1;   //! should be green in board.
-      }
+    for(int i = 2; i < moves.length() ; i += 2)
+    {
+      int dstRow = moves.charAt(i) - 'a';
+      int dstCol = 8 - (moves.charAt(i+1) - '0');
+        red[dstRow][dstCol] = 1;   //! should be green in board.
     }
+  }
 }
  
 void setup() {
@@ -129,26 +128,26 @@ void setup() {
     pinMode(rowPins[i], OUTPUT);
     digitalWrite(rowPins[i], LOW);
   }
+
   pinMode(greenDataPin, OUTPUT);
   pinMode(greenClockPin, OUTPUT);
+
   pinMode(redDataPin, OUTPUT);
   pinMode(redClockPin, OUTPUT);
 
-  pinMode(buttonW,INPUT_PULLUP);
-  pinMode(buttonB,INPUT_PULLUP);
-  pinMode(buttonH,INPUT_PULLUP);
 
-  //attachInterrupt(digitalPinToInterrupt(buttonW), handleButtonW, FALLING);
-  //attachInterrupt(digitalPinToInterrupt(buttonB), handleButtonB, FALLING);
+  pinMode(buttonW,INPUT_PULLUP);
+  pinMode(buttonH,INPUT_PULLUP);
+  pinMode(buttonR,INPUT_PULLUP);
+  pinMode(buttonB,INPUT_PULLUP);
 
 }
 
 void loop() {
 
   updateDisplayMatrix();
-  //delay(10); //100 (recomended value)
-  //Serial.println(digitalRead(buttonW));
 
+  // ---- BUTTON W ---- (White)
   bool readingW = digitalRead(buttonW);
   if (readingW != lastButtonWState) {
       if (lastButtonWState == HIGH && readingW == LOW) {
@@ -158,7 +157,27 @@ void loop() {
   }
   lastButtonWState = readingW;
 
-  // --- BUTTON B ---
+  // ---- BUTTON H ---- (Help)
+  bool readingH = digitalRead(buttonH);
+  if (readingH != lastButtonHState) {
+      if (lastButtonHState == HIGH && readingH == LOW) {
+        handleButtonH();
+        delay(200);
+    }
+  }
+  lastButtonHState = readingH;
+
+  // ---- BUTTON R ---- (Resign)
+  bool readingR = digitalRead(buttonR);
+  if (readingR != lastButtonRState) {
+      if (lastButtonRState == HIGH && readingR == LOW) {
+        handleButtonR();
+        delay(200);
+    }
+  }
+  lastButtonRState = readingR;
+
+  // ---- BUTTON B ---- (Black)
   bool readingB = digitalRead(buttonB);
   if (readingB != lastButtonBState) {
       if (lastButtonBState == HIGH && readingB == LOW) {
@@ -167,18 +186,5 @@ void loop() {
     }
   }
   lastButtonBState = readingB;
-
-  bool readingH = digitalRead(buttonH);
-  if (readingH != lastButtonBState) {
-      if (lastButtonHState == HIGH && readingH == LOW) {
-        handleButtonH();
-        delay(200);
-    }
-  }
-  lastButtonHState = readingH;
   
 }
-
-
-
-
